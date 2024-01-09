d90664f31dd47d8f5da655b86fc6737a104c7d22
Luis Munoz Diaz <luismunozdiaz@icloud.com>
2024-01-08 23:25:04 -0600

    colorscheme updates; work on completions

--- lua/lmunozdiaz/plugins/colorscheme.lua
+++ lua/lmunozdiaz/plugins/colorscheme.lua
@@ -1,16 +1,26 @@
 local M = {
-  "rose-pine/neovim",
-  name = 'rose-pine',
-  opts = {
-    highlight_groups = {
-      CursorLine = { bg = "none" },
-      MatchParen = { bg = "base", fg = "love" },
-    },
-  },
-  config = function(_, opts)
-    require("rose-pine").setup(opts)
-    vim.cmd('colorscheme rose-pine')
-  end,
+	"rose-pine/neovim",
+	name = "rose-pine",
+	opts = {
+		highlight_groups = {
+			CursorLine = { bg = "none" },
+			MatchParen = { bg = "base", fg = "love" },
+			MiniStarterHeader = { fg = "love" },
+			MiniStarterFooter = { fg = "rose" },
+			MiniFilesBorder = { fg = "overlay" },
+			MiniNotifyBorder = { fg = "overlay" },
+			MiniPickBorder = { fg = "overlay" },
+			MiniFilesNormal = { bg = "base" },
+			MiniNotifyNormal = { bg = "base" },
+			MiniPickNormal = { bg = "base" },
+			MiniNotifyTitle = { fg = "foam" },
+			MiniFilesDirectory = { fg = "rose" },
+		},
+	},
+	config = function(_, opts)
+		require("rose-pine").setup(opts)
+		vim.cmd("colorscheme rose-pine")
+	end,
 }
 
 return M
