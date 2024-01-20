47ee62ff714373bcc5506d47c697c9c39f6b2889
Luis Munoz Diaz <luismunozdiaz@icloud.com>
2024-01-20 10:29:19 -0600

    bracket pair colorization

--- init.lua
+++ init.lua
@@ -23,3 +23,23 @@ if vim.g.vscode then
 else
 	require("lmunozdiaz.config").setup()
 end
+
+local rainbow = require("rainbow-delimiters")
+local highlight = vim.api.nvim_set_hl
+vim.g.rainbow_delimiters = {
+	strategy = {
+		html = rainbow.strategy["noop"],
+	},
+	query = {
+		javascript = { "rainbow-parens" },
+	},
+	highlight = {
+		highlight(0, "RainbowDelimiterRed", { fg = "#ffd602" }),
+		highlight(0, "RainbowDelimiterYellow", { fg = "#da70d6" }),
+		highlight(0, "RainbowDelimiterBlue", { fg = "#169fff" }),
+		highlight(0, "RainbowDelimiterOrange", { fg = "#da70d6" }),
+		highlight(0, "RainbowDelimiterGreen", { fg = "#169fff" }),
+		highlight(0, "RainbowDelimiterViolet", { fg = "#169fff" }),
+		highlight(0, "RainbowDelimiterCyan", { fg = "#ffd602" }),
+	},
+}
--- lua/lmunozdiaz/plugins/editor.lua
+++ lua/lmunozdiaz/plugins/editor.lua
@@ -1,93 +1,96 @@
 local M = {
-	-- {
-	-- 	"nvim-treesitter/nvim-treesitter-context",
-	-- 	opts = {
-	-- 		separator = nil,
-	-- 	},
-	-- 	config = function(_, opts)
-	-- 		require("treesitter-context").setup(opts)
-	-- 	end,
-	-- },
-	{
-		"utilyre/sentiment.nvim",
-		version = "*",
-		event = "VeryLazy",
-		opts = {
-			pairs = {
-				{ "(", ")" },
-				{ "{", "}" },
-				{ "[", "]" },
-				{ "<", ">" },
-			},
-		},
-		init = function()
-			vim.g.loaded_matchparen = 1
-		end,
-	},
-	{
-		"windwp/nvim-ts-autotag",
-		opts = {},
-		config = function(_, opts)
-			require("nvim-ts-autotag").setup(opts)
-		end,
-	},
-	{
-		"abecodes/tabout.nvim",
-		opts = {},
-		config = function(_, opts)
-			require("tabout").setup(opts)
-		end,
-	},
-	{
-		"echasnovski/mini.comment",
-		version = "*",
-		opts = {
-			ignore_blank_line = true,
-		},
-		config = function(_, opts)
-			require("mini.comment").setup(opts)
-		end,
-	},
-	{
-		"echasnovski/mini.pairs",
-		version = "*",
-		opts = {},
-		config = function(_, opts)
-			require("mini.pairs").setup(opts)
-		end,
-	},
-	{
-		"echasnovski/mini.surround",
-		version = "*",
-		opts = {},
-		config = function(_, opts)
-			require("mini.surround").setup(opts)
-		end,
-	},
-	{
-		"echasnovski/mini.jump",
-		version = "*",
-		opts = {},
-		config = function(_, opts)
-			require("mini.jump").setup(opts)
-		end,
-	},
-	{
-		"echasnovski/mini.move",
-		version = "*",
-		opts = {},
-		config = function(_, opts)
-			require("mini.move").setup(opts)
-		end,
-	},
-	{
-		"echasnovski/mini.cursorword",
-		version = "*",
-		opts = {},
-		config = function(_, opts)
-			require("mini.cursorword").setup(opts)
-		end,
-	},
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter-context",
+  -- 	opts = {
+  -- 		separator = nil,
+  -- 	},
+  -- 	config = function(_, opts)
+  -- 		require("treesitter-context").setup(opts)
+  -- 	end,
+  -- },
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+  },
+  {
+    "utilyre/sentiment.nvim",
+    version = "*",
+    event = "VeryLazy",
+    opts = {
+      pairs = {
+        { "(", ")" },
+        { "{", "}" },
+        { "[", "]" },
+        { "<", ">" },
+      },
+    },
+    init = function()
+      vim.g.loaded_matchparen = 1
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    opts = {},
+    config = function(_, opts)
+      require("nvim-ts-autotag").setup(opts)
+    end,
+  },
+  {
+    "abecodes/tabout.nvim",
+    opts = {},
+    config = function(_, opts)
+      require("tabout").setup(opts)
+    end,
+  },
+  {
+    "echasnovski/mini.comment",
+    version = "*",
+    opts = {
+      ignore_blank_line = true,
+    },
+    config = function(_, opts)
+      require("mini.comment").setup(opts)
+    end,
+  },
+  {
+    "echasnovski/mini.pairs",
+    version = "*",
+    opts = {},
+    config = function(_, opts)
+      require("mini.pairs").setup(opts)
+    end,
+  },
+  {
+    "echasnovski/mini.surround",
+    version = "*",
+    opts = {},
+    config = function(_, opts)
+      require("mini.surround").setup(opts)
+    end,
+  },
+  {
+    "echasnovski/mini.jump",
+    version = "*",
+    opts = {},
+    config = function(_, opts)
+      require("mini.jump").setup(opts)
+    end,
+  },
+  {
+    "echasnovski/mini.move",
+    version = "*",
+    opts = {},
+    config = function(_, opts)
+      require("mini.move").setup(opts)
+    end,
+  },
+  {
+    "echasnovski/mini.cursorword",
+    version = "*",
+    opts = {},
+    config = function(_, opts)
+      require("mini.cursorword").setup(opts)
+    end,
+  },
 }
 
 return M
