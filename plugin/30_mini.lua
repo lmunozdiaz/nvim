51aec2467db3a00feba79b85414c9c5e78f51a9f
Luis Munoz Diaz <lemunoz8@gmail.com>
2026-03-31 19:39:13 -0500

    revert: set config state to newest MiniMax nvim-0.12

--- plugin/30_mini.lua
+++ plugin/30_mini.lua
@@ -27,27 +27,18 @@
 local now, now_if_args, later = Config.now, Config.now_if_args, Config.later
 
 -- Step one ===================================================================
--- Create a custom colorscheme. It uses 'mini.hues', which comes with 'mini.nvim'.
+-- Enable 'miniwinter' color scheme. It comes with 'mini.nvim' and uses 'mini.hues'.
 --
 -- See also:
 -- - `:h mini.nvim-color-schemes` - list of other color schemes
 -- - `:h MiniHues-examples` - how to define highlighting with 'mini.hues'
 -- - 'plugin/40_plugins.lua' honorable mentions - other good color schemes
-now(function()
-  require('mini.hues').setup({
-    foreground = '#ffffff',
-    background = '#1c1c1f',
-    saturation = 'high',
-  })
-  -- Custom overrides
-  vim.api.nvim_set_hl(0, 'MiniTablineFill', { link = 'MiniTablineCurrent' })
-end)
+now(function() vim.cmd('colorscheme miniwinter') end)
 
 -- You can try these other 'mini.hues'-based color schemes (uncomment with `gcc`):
 -- now(function() vim.cmd('colorscheme minispring') end)
 -- now(function() vim.cmd('colorscheme minisummer') end)
 -- now(function() vim.cmd('colorscheme miniautumn') end)
--- now(function() vim.cmd('colorscheme miniwinter') end)
 -- now(function() vim.cmd('colorscheme randomhue') end)
 
 -- Common configuration presets. Example usage:
@@ -435,7 +426,7 @@ later(function() require('mini.cmdline').setup() end)
 --
 -- It is not enabled by default because it is not really needed on a daily basis.
 -- Uncomment next line (use `gcc`) to enable.
-later(function() require('mini.colors').setup() end)
+-- later(function() require('mini.colors').setup() end)
 
 -- Comment lines. Provides functionality to work with commented lines.
 -- Uses `:h 'commentstring'` option to infer comment structure.
